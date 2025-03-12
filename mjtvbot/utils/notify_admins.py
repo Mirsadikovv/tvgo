dccfbc845db320c629735bea647a3f3b3b9505b8
mirodil <mirsadikovmirodil52@gmail.com>
2025-03-12 10:41:41 +0500

    add some fix

--- mjtvbot/utils/notify_admins.py
+++ mjtvbot/utils/notify_admins.py
@@ -9,7 +9,7 @@ async def on_startup_notify(dp: Dispatcher):
     photo = "https://avatars.mds.yandex.net/i?id=f97c1c9da4b2706eec172e9e009e9ee3-5870104-images-thumbs&n=13"
     for admin in ADMINS:
         try:
-            await dp.bot.send_photo(admin,photo,caption="Бот начал работу  /start\n\nКоманды для админов:\n/admin - админ панель")
+            await dp.bot.send_photo(admin,photo,caption="Бот начал работуууу  /start\n\nКоманды для админов:\n/admin - админ панель")
 
         except Exception as err:
             logging.exception(err)
