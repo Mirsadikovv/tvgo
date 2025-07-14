67f5ff918a69ff154985d6811b68592f156d4dbe
mirodil <mirsadikovmirodil52@gmail.com>
2025-07-14 15:25:15 +0500

    photo

--- mjtvbot/handlers/users/tarif.py
+++ mjtvbot/handlers/users/tarif.py
@@ -6,17 +6,36 @@ import logging
 from loader import dp
 
 
+# @dp.message_handler(text='VIP')
+# async def send_link(message: Message):
+#     photo = "AgACAgIAAxkBAAIBoGgbnuXe2GCs1I6_LRCbToNfHnT_AAIu7zEb1xrhSAMVU-4wX99eAQADAgADeAADNgQ"
+#     caption =  "В тариф VIP входит\n\n"
+    
+#     caption += "Цена: <b>22 000 сум</b>\n"
+#     caption += "Скидка от админа: <b>-5 000 сум</b>\n"
+#     caption += "Итого к оплате: 17 000\n\n"
+#     caption += "Для покупки нажмите👇" 
+#     await message.answer_photo(photo = photo,caption = caption,reply_markup=build_keyboard("VIP"))
+
 @dp.message_handler(text='VIP')
 async def send_link(message: Message):
-    photo = "AgACAgIAAxkBAAIBoGgbnuXe2GCs1I6_LRCbToNfHnT_AAIu7zEb1xrhSAMVU-4wX99eAQADAgADeAADNgQ"
-    caption =  "В тариф VIP входит\n\n"
-    
-    caption += "Цена: <b>22 000 сум</b>\n"
-    caption += "Скидка от админа: <b>-5 000 сум</b>\n"
-    caption += "Итого к оплате: 17 000\n\n"
-    caption += "Для покупки нажмите👇" 
-    await message.answer_photo(photo = photo,caption = caption,reply_markup=build_keyboard("VIP"))
+    photo_path = "vip.png"  # если файл лежит рядом с этим скриптом
+    caption = (
+        "В тариф VIP входит\n\n"
+        "Цена: <b>22 000 сум</b>\n"
+        "Скидка от админа: <b>-5 000 сум</b>\n"
+        "Итого к оплате: 17 000\n\n"
+        "Для покупки нажмите👇"
+    )
 
+    with open(photo_path, "rb") as photo:
+        await message.answer_photo(
+            photo=photo,
+            caption=caption,
+            parse_mode="HTML",
+            reply_markup=build_keyboard("VIP")
+        )
+        
 @dp.message_handler(text='Sport')
 async def send_link(message: Message):
     photo = "AgACAgIAAxkBAAIBomgbnxMqDrIBd1WybWWKccDWS64TAAKe8zEbv8bhSBrUvO625YPgAQADAgADeAADNgQ"
