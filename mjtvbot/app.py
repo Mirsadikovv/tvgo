1ddd343b55da72a999203d8888738af6045a063d
mirodil <mirsadikovmirodil52@gmail.com>
2025-07-14 14:56:57 +0500

    update commands

--- mjtvbot/app.py
+++ mjtvbot/app.py
@@ -7,7 +7,6 @@ from utils.set_bot_commands import set_default_commands
 
 async def on_startup(dispatcher):
     await db.create()
-    # await db.drop_users()https://tbs01-edge11.itdc.ge/ort/tracks-v1a1/mono.m3u8
     await db.create_table_users()
 
     await set_default_commands(dispatcher)
--- mjtvbot/utils/set_bot_commands.py
+++ mjtvbot/utils/set_bot_commands.py
@@ -1,10 +1,27 @@
 from aiogram import types
 
+from environs import Env
+
+env = Env()
+env.read_env()
+
+admins = env.str("ADMINS")
 
 async def set_default_commands(dp):
-    await dp.bot.set_my_commands(
-        [
-            types.BotCommand("start", "Начать работу"),
-            types.BotCommand("help", "Помощь"),
-        ]
-    )
+    current_user = await dp.bot.get_me()
+    if current_user in admins:
+        await dp.bot.set_my_commands(
+            [
+                types.BotCommand("start", "Начать работу"),
+                types.BotCommand("help", "Помощь"),
+                types.BotCommand("admin", "Админ панель"),
+            ]
+        )
+    else:
+        await dp.bot.set_my_commands(
+            [
+                types.BotCommand("start", "Начать работу"),
+                types.BotCommand("help", "Помощь"),
+            ]
+        )
+
