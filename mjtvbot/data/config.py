7f6e5e981e875364d951fee0d9e9c84445598e6c
mirodil <mirsadikovmirodil52@gmail.com>
2025-07-14 16:27:02 +0500

    get from secrets

--- mjtvbot/data/config.py
+++ mjtvbot/data/config.py
@@ -1,10 +1,11 @@
 from environs import Env
 
+import os 
 env = Env()
 env.read_env()
 
 
-BOT_TOKEN = env.str("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
 IP = env.str("ip")
 
